d5937eb3c659f27769ee2ce2120b60a7b586aea1
silver23rain <silver23rain@gmail.com>
2018-10-25 21:28:47 +0900

    [기록 작성] 상단에 오늘 날짜 추가

--- src/components/record/RecordPageTitle.js
+++ src/components/record/RecordPageTitle.js
@@ -1,15 +1,32 @@
 import * as React from 'react';
+import CalandarImg from "./resource/cal.png"
 import PageTitle from '../setting/PageTitle';
 import { PageSubTitle } from './RecordStyle';
 import PageEditButton from './PageEditButton';
 
 class RecordPageTitle extends React.Component {
     render() {
-        
+        const date = new Date();
+        const dateSting = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        const style = {
+            float: "right",
+            backgroundImage: `url(${CalandarImg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "3px 1px",
+            paddingLeft: "24px",
+            color: "#565656",
+            position: "relative",
+            top: "58px"
+        }
+
         return (
             <div>
-                <PageTitle title="기록 작성"/>
+                <PageTitle title="기록 작성" />
                 <PageSubTitle>하루를 채워보세요</PageSubTitle>
+                <PageSubTitle style={style}>
+                    <b>오늘 : </b>
+                    <span>{dateSting}</span>
+                </PageSubTitle>
             </div>
         );
     }
