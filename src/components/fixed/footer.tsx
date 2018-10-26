c4f533298062a060f2bbf810e8aa5dc9588d2ff4
wurikiji <wurikiji@gmail.com>
2018-10-27 00:34:07 +0900

    First commit for header/footer

---
+++ src/components/fixed/footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Footer extends React.Component {
+    public render() {
+        return (
+            <div>
+                footer 에오
+            </div>
+        );
+    }
+} 
---
+++ src/components/fixed/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default class Header extends React.Component {
+    public render() {
+        return (
+            <div>
+                header 에오
+            </div>
+        );
+    }
+}
